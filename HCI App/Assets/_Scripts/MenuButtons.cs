724b9fa88dfa7f461b0fc4d5056ea9dbae2fb830
Unknown <robert.andersen@uoit.net>
2020-11-23 21:07:41 -0500

    Added an example button with script so y'all don't have to google how buttons work <3 Script is commented btw

---
+++ HCI App/Assets/_Scripts/MenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    //Name of the scene to switch to
+    public string test;
+
+    //Just the example test button, but this button stuff isd all pretty ez!
+    public void _ButtonNameHere_OnClick()
+    {
+        //This just cvhanges the scene to whatever test is set as in the editor
+        SceneManager.LoadScene(test, LoadSceneMode.Single);
+    }
+}
